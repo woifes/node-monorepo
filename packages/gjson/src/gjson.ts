eb35595be7bb0e2d949689f6fdee6ad5e0dfcf1a
woifes <81268775+woifes@users.noreply.github.com>
2023-10-28 23:23:30 +0200

    Added dockstring

--- packages/gjson/src/gjson.ts
+++ packages/gjson/src/gjson.ts
@@ -4,18 +4,24 @@
 import { handleNextLevel } from "./path/handleNextLevel";
 import { splitPath } from "./path/splitPath";
 
+/**
+ * The function searches in the given data for values via the GJSON path syntax
+ * @param data the data to search in. Can be a string which gets parsed to JSON or JS object.
+ * @param path the GJSON search path
+ * @returns
+ */
 export function get(data: any, path: string) {
-    let dataObj: any;
-    if (typeof data === "string") {
-        dataObj = JSON.parse(data);
-    } else {
-        dataObj = data;
-    }
+	let dataObj: any;
+	if (typeof data === "string") {
+		dataObj = JSON.parse(data);
+	} else {
+		dataObj = data;
+	}
 
-    const pathItems = splitPath(path);
-    try {
-        return handleNextLevel(dataObj, pathItems);
-    } catch {
-        return undefined;
-    }
+	const pathItems = splitPath(path);
+	try {
+		return handleNextLevel(dataObj, pathItems);
+	} catch {
+		return undefined;
+	}
 }
